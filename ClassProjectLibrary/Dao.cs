6887ed98a24d3e5bf704fc1f911bd2868e15c5df
bill <nafar.belal@gmail.com>
2019-02-28 11:45:41 +0100

    update RequestCondition

--- ClassProjectLibrary/Dao.cs
+++ ClassProjectLibrary/Dao.cs
@@ -7,13 +7,15 @@ using MySql.Data.MySqlClient;
 
 namespace ClassProjectLibrary
 {
-    public class Dao : MySql
+    public abstract class Dao : MySql
     {
         public string[] param_co;
         string Table;
 
         public Dao(string table, string db, string user, string pass, string server) : base(db, user, pass, server)
         {
+            param_co = new string[]{ db, user, pass, server};
+
             Table = table;
         }
 
@@ -26,62 +28,53 @@ namespace ClassProjectLibrary
 
             return Get(sql);
         }
-
-        public int Update(string action, List<Dictionary<string, string>> Data, string Conditions)
+        public int insert(List<Dictionary<string, string>> Data)
         {
             string sql = "";
             string att;
             string values;
 
-            action = action.ToLower();
+            sql = "insert into " + Table;
 
-            switch (action)
+            foreach (var data in Data)
             {
-                case "insert":
-                    sql = "insert into " + Table;
-                    foreach (var data in Data)
-                    {
-                        att = "";
-                        values = "";
-                        foreach (KeyValuePair<string, string> entry in data)
-                        {
-                            att += entry.Key + ",";
-                            values += "'" + entry.Value + "',";
-                        }
+                att = "";
+                values = "";
+                foreach (KeyValuePair<string, string> entry in data)
+                {
+                    att += entry.Key + ",";
+                    values += "'" + entry.Value + "',";
+                }
 
-                        att = att.Remove(att.Length - 1);
-                        values = values.Remove(values.Length - 1);
-                        Up(sql + "(" + att + ") values (" + values + ")");
-                    }
-                    break;
+                att = att.Remove(att.Length - 1);
+                values = values.Remove(values.Length - 1);
+                Up(sql + "(" + att + ") values (" + values + ")");
+            }
+            return 1;
+        }
 
-                case "update":
-                    foreach (var data in Data)
-                    {
-                        sql = "update " + Table + " set ";
-                        foreach (KeyValuePair<string, string> entry in data)
-                        {
-                            sql += entry.Key + "='" + entry.Value + "',";
-                        }
+        public int update(Dictionary<string, string> Data, string Conditions)
+        {
+            string sql = "update " + Table + " set ";
 
-                        sql = sql.Remove(sql.Length - 1);
-                        sql += " where " + Conditions;
-                        Up(sql);
-                    }
-                    break;
-
-                case "delete":
-                    sql = "delete from " + Table + " where " + Conditions;
-                    Up(sql);
-                    break;
-
-                default: return -1;
+            foreach (KeyValuePair<string, string> entry in Data)
+            {
+                sql += entry.Key + "='" + entry.Value + "',";
             }
 
-            return 1;
+            sql = sql.Remove(sql.Length - 1);
 
-            //update table set DATA where conditions 
-            //delete from table where 
+            sql += " where " + Conditions;
+
+            return Up(sql);
         }
+
+        public int delete(string Conditions)
+        {
+            string sql = "delete from " + Table + " where " + Conditions;
+
+            return Up(sql);
+        }
+
     }
 }
--- ClassProjectLibrary/RequestCondition.cs
+++ ClassProjectLibrary/RequestCondition.cs
@@ -9,13 +9,16 @@ namespace ClassProjectLibrary
     {
         string Param;
 
-        public RequestCondition()
+        public string Param1 { get => Param; set => Param = value; }
+
+        public RequestCondition(string p)
         {
+            Param = p;
         }
 
         public string Equal(object value)
-        {
-            return null;
+        {            
+            return Param1 + "='" + value.ToString() + "'";
         }
 
         private string ForINnotIN(object[] value)
@@ -25,52 +28,81 @@ namespace ClassProjectLibrary
 
         public string GreaterThan(object value)
         {
-            return null;
+            return Param1 + ">'" + value.ToString() + "'";
         }
 
         public string GreaterThanOrEqual(object[] value)
         {
-            return null;
+            return Param1 + ">='" + value.ToString() + "'";
         }
 
         public string IN(object[] value)
         {
-            return null;
+            //format giveen array as 
+            //param in ('val1','val2','val3')
+
+            string cond=Param+" IN (";
+            foreach(var a in value)
+            {
+                cond += "'" +a.ToString()+ "',";
+            }
+            //remove the last , from cond
+
+            cond=cond.Remove(cond.Length - 1);
+
+            cond += ")";
+
+            return cond;
         }
 
         public string LessThan(object value)
         {
-            return null;
+            return Param1 + "<'" + value.ToString() + "'";
         }
 
         public string LessThanOrEqual(object value)
         {
-            return null;
+            return Param1 + "<='" + value.ToString() + "'";
         }
 
         public string LikeCenter(object value)
         {
-            return null;
+            return Param1 + " like '%" + value.ToString() + "%'";
         }
 
         public string LikeFirst(object value)
         {
-            return null;
+            return Param1 + " like '" + value.ToString() + "%'";
         }
 
         public string LikeLast(object value)
         {
-            return null;
+            return Param1 + " like '%" + value.ToString() + "'";
         }
 
         public string NotEqual(object value)
         {
-            return null;
+            return Param1 + "<> '" + value.ToString() + "'";
         }
 
         public string NotIN(object[] value)
         {
-            return null;
+            //format giveen array as 
+            //param in ('val1','val2','val3')
+
+            string cond = Param + " NOT IN (";
+            foreach (var a in value)
+            {
+                cond += "'" + a.ToString() + "',";
+            }
+            //remove the last , from cond
+
+            cond = cond.Remove(cond.Length - 1);
+
+            cond += ")";
+
+            return cond;
+
         }
 
 
